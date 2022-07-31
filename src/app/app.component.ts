09a585c14e095e5430d7baaf436d52750cfee748
SoojinKang0127 <iamhajaml@naver.com>
2022-07-31 22:00:41 +0900

    Done with Attribute Directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'teamapp';
+  newMemberName: string = '';
+  members: string[] = [];
+
+  addMember() {
+    this.members.push(this.newMemberName);
+    this.newMemberName = '';
+  }
+
+  onInput(member: string) {
+    this.newMemberName = member;
+  }
 }
